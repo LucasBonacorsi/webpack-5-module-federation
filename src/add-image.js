76aadc9eb870ad8e086482b6967ef528f6bdb656
Lucas Bonacorsi <lucasbonacorsi@hotmail.com>
2023-02-26 17:24:40 -0300

    eslint added

--- src/add-image.js
+++ src/add-image.js
@@ -5,7 +5,6 @@ function addImage() {
     const img = document.createElement('img');
     img.alt = altText;
     img.width = 300;
-    img.src = cat
     const body = document.querySelector('body')
     body.appendChild(img)
 }
--- src/cat.js
+++ src/cat.js
@@ -1,6 +1,5 @@
 import Heading from "./components/heading/heading";
 import CatImage from "./components/cat-image/cat-image";
-import React from 'react';
 
 const heading = new Heading();
 heading.render('cat');
